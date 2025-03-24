dc8902dd7a44fabca408c4441e6d15a95b1406eb
Sarmad Ali <132395342+sarmadalikhanofficial@users.noreply.github.com>
2025-03-24 20:24:06 +0500

    Create 01.java

---
+++ practice/JFbyJB/01.java
@@ -0,0 +1,27 @@
+class Answer {
+
+    static boolean showExpectedResult = true;
+    static boolean showHints = false;
+
+    static String fillInTheBlank(String season, int numberOfCups, String adjective) {
+        String story = "On a " + adjective + " " + season + " day, I drink a minimum of " + numberOfCups + " cups of coffee.";
+        return story;
+    }
+
+}
+
+class Main{
+    public static void main(String[] args){
+        String season = "winter";
+        int numberOfCups = 4;
+        String adjective = "cold";
+        String result = Answer.fillInTheBlank(season, numberOfCups, adjective);
+        System.out.println(result);
+    }
+}
+
+
+// Parameters
+// season : A string representing a season
+// numberOfCups : An integer representing the number of cups of coffee
+// adjective : A string representing an adjective
