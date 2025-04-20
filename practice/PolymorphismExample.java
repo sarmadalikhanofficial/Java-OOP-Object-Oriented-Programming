a34aacd8e3129084bb2126dbc842c87c538278c7
sarmadalikhanofficial <sarmadali074@gmail.com>
2025-04-21 03:14:05 +0500

    polymorph.. added

---
+++ practice/PolymorphismExample.java
@@ -0,0 +1,38 @@
+// Parent class
+class Shape {
+    public void draw() {
+        System.out.println("Drawing a shape");
+    }
+}
+
+// Child class 1
+class Circle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a circle");
+    }
+}
+
+// Child class 2
+class Square extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a square");
+    }
+}
+
+// Main class
+public class PolymorphismExample {
+    public static void main(String[] args) {
+        Shape myShape;         // reference of parent class
+
+        Shape shape = new Shape();
+        shape.draw(); 
+
+        myShape = new Circle();  // object of Circle
+        myShape.draw();          // Output: Drawing a circle
+
+        myShape = new Square();  // object of Square
+        myShape.draw();          // Output: Drawing a square
+    }
+}
