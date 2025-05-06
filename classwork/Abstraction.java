a1c774fd71e18487eda9551a15d61442c3e6ad84
sarmadalikhanofficial <sarmadali074@gmail.com>
2025-05-06 09:22:37 +0500

    classwork

---
+++ classwork/Abstraction.java
@@ -0,0 +1,66 @@
+public class Abstraction {
+
+    // Abstract class representing a Shape
+    abstract static class Shape {
+        // Abstract method to calculate area
+        public abstract double calculateArea();
+
+        // Concrete method to display information
+        public void display() {
+            System.out.println("This is a shape.");
+        }
+    }
+
+    // Concrete class representing a Circle
+    static class Circle extends Shape {
+        private double radius;
+
+        public Circle(double radius) {
+            this.radius = radius;
+        }
+
+        @Override
+        public double calculateArea() {
+            return Math.PI * radius * radius;
+        }
+
+        @Override
+        public void display() {
+            System.out.println("This is a circle with radius " + radius);
+        }
+    }
+
+    // Concrete class representing a Rectangle
+    static class Rectangle extends Shape {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width) {
+            this.length = length;
+            this.width = width;
+        }
+
+        @Override
+        public double calculateArea() {
+            return length * width;
+        }
+
+        @Override
+        public void display() {
+            System.out.println("This is a rectangle with length " + length + " and width " + width);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Creating instances of Circle and Rectangle
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(4, 6);
+
+        // Calling methods to display information and calculate area
+        circle.display();
+        System.out.println("Area of the circle: " + circle.calculateArea());
+
+        rectangle.display();
+        System.out.println("Area of the rectangle: " + rectangle.calculateArea());
+    }
+}
