a347eefb5f66b6dff3649138496a1a0368807ba0
sarmadalikhanofficial <sarmadali074@gmail.com>
2025-05-20 10:23:36 +0500

    may20 interface

--- classwork/May20/Interface.java
+++ classwork/May20/Interface.java
@@ -1,3 +1,4 @@
+// defining the interface
 interface Callback {
     void callback(int val);
 }
@@ -10,41 +11,54 @@ interface smithkline {
     void calpol(int val);
 }
 
+// implementing Callback interface
 class Client implements Callback {
     public void callback(int p) {
-        System.out.println("We have to arrange a callback at " + p);
+        System.out.println("Client: We have to arrange a callback at " + p);
     }
 }
 
+// another class implementing the same interface Callback
 class AnotherClient implements Callback {
     public void callback(int p) {
-        System.out.println("Another version of callback");
-        System.out.println("p squared is " + (p * p));
+        System.out.println("AnotherClient: Another version of callback");
+        System.out.println("AnotherClient: Squared of value is " + (p * p));
     }
 }
 
+// implementing multiple interfaces
 class MyMergerClient implements glaxo, smithkline {
-    public void panadol(int val) {
-        System.out.println("Glaxo: Panadol dose is " + val + "mg");
+    public void calpol(int val) {
+        System.out.println("MyMergerClient: Revenue generated for Calpol is " + val + "ml");
     }
 
-    public void calpol(int val) {
-        System.out.println("SmithKline: Calpol syrup dose is " + val + "ml");
+    public void panadol(int val) {
+        System.out.println("MyMergerClient: Top most sale of the year for Panadol will be " + val + "mg");
     }
 }
 
-public class Interface {
+
+// main class to run everything
+public class Interface{
     public static void main(String[] args) {
-        // Callback interface test
-        Callback c1 = new Client();
-        c1.callback(10);
 
-        Callback c2 = new AnotherClient();
-        c2.callback(5);
+        glaxo c = new MyMergerClient();
+        smithkline d = new MyMergerClient();
+        MyMergerClient obj = new MyMergerClient();
+        d.calpol(10);
+        c.panadol(500);
+        obj.panadol(500);
+        obj.calpol(10);
 
-        // Testing multiple interface implementation
-        MyMergerClient merger = new MyMergerClient();
-        merger.panadol(500);
-        merger.calpol(10);
+        // Demonstrating the Callback interface
+        Client clientObj = new Client();
+        AnotherClient anotherClientObj = new AnotherClient();
+
+        // You would typically pass these client objects to another class
+        // that would then call the callback method. For this example,
+        // we'll just call them directly.
+        System.out.println("\nDemonstrating Callback Interface:");
+        clientObj.callback(100);
+        anotherClientObj.callback(200);
     }
 }
